4a4f4800148053459c420ee1f45fa3a437a9d930
cvpcs <root@cvpcs.org>
2010-06-16 21:05:56 -0500

    added future possibility of port knocking daemon

--- Android.mk
+++ Android.mk
@@ -1,17 +1,19 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES:=\
+LOCAL_SRC_FILES := \
 	src/knock.c
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 LOCAL_MODULE := knock
 include $(BUILD_EXECUTABLE)
 
-# relies on libpcap
 #include $(CLEAR_VARS)
-#LOCAL_SRC_FILES:=\
+#LOCAL_SRC_FILES := \
 #	src/knockd.c \
 #	src/list.c
+#LOCAL_C_INCLUDES := \
+#	external/libpcap
+#LOCAL_SHARED_LIBRARIES := libpcap
 #LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 #LOCAL_MODULE := knockd
 #include $(BUILD_EXECUTABLE)
